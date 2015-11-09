229e0c5295042ad617d49d5092f1c8054b85a369
Honig <gundamlh@hotmail.com>
2015-11-09 13:41:08 +0800

    Initial commit

---
+++ hetServo_RM57L/Debug/source/HL_errata.pp
@@ -0,0 +1,33 @@
+# FIXED
+
+source/HL_errata.obj: ../source/HL_errata.c
+source/HL_errata.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_errata.h
+source/HL_errata.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_reg_pbist.h
+source/HL_errata.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_sys_common.h
+source/HL_errata.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_hal_stdtypes.h
+source/HL_errata.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h
+source/HL_errata.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h
+source/HL_errata.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h
+source/HL_errata.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h
+source/HL_errata.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h
+source/HL_errata.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h
+source/HL_errata.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_reg_system.h
+source/HL_errata.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_reg_gio.h
+source/HL_errata.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_sys_core.h
+source/HL_errata.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_sys_pmu.h
+
+../source/HL_errata.c: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_errata.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_reg_pbist.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_sys_common.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_hal_stdtypes.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_reg_system.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_reg_gio.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_sys_core.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_sys_pmu.h: 
---
+++ hetServo_RM57L/Debug/source/HL_pinmux.pp
@@ -0,0 +1,25 @@
+# FIXED
+
+source/HL_pinmux.obj: ../source/HL_pinmux.c
+source/HL_pinmux.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_pinmux.h
+source/HL_pinmux.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_reg_pinmux.h
+source/HL_pinmux.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_sys_common.h
+source/HL_pinmux.obj: E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_hal_stdtypes.h
+source/HL_pinmux.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h
+source/HL_pinmux.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h
+source/HL_pinmux.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h
+source/HL_pinmux.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h
+source/HL_pinmux.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h
+source/HL_pinmux.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h
+
+../source/HL_pinmux.c: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_pinmux.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_reg_pinmux.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_sys_common.h: 
+E:/Documents/Workspaces/ti/CCS/hetServo_RM57L/include/HL_hal_stdtypes.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h: 
+D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h: 
